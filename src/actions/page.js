bfa063483c9a93d8da73b0d8470bb3c78fb9f4ef
aufimtsev <avufimtsev.sbt@sberbank.ru>
2016-07-22 14:48:07 +0600

    action types constants

--- src/actions/page.js
+++ src/actions/page.js
@@ -1,6 +1,8 @@
+import { SET_YEAR } from '../constants/actionTypes'
+
 export function setYear(year) {
     return {
-        type: 'SET_YEAR',
+        type: SET_YEAR,
         payload: year
     }
 }
--- src/reducers/page.js
+++ src/reducers/page.js
@@ -1,3 +1,5 @@
+import { SET_YEAR } from '../constants/actionTypes'
+
 const initialState = {
 	year: 2016,
 	photos: []
@@ -5,7 +7,7 @@ const initialState = {
 
 export default function page(state = initialState, action) {
 	switch (action.type) {
-        case 'SET_YEAR':
+        case SET_YEAR:
             return { ...state, year: action.payload }
         default:
             return state
